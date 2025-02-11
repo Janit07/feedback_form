28bc95488f5f2f5d16152dd4b8aa337f17936253
Janit Sharma <49781243+Janit07@users.noreply.github.com>
2025-02-11 09:23:03 +0530

    Update FeedbackForm.jsx

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -2,16 +2,49 @@ import React, { useState } from 'react';
 import './FeedbackForm.css'; // Import CSS for styling
 
 const FeedbackForm = () => {
+        const [formData, setFormData]= useState({
+            name: '',
+            email: '',
+            feedback: ''
+        });
+
+        const handleChange=(event)=>{
+                    const {name,value}=event.target;
+                    setFormData({...formData,[name]: value});
+        };
+
+        const handleSubmit=(event)=>{
+            event.preventDefault();
+            const confirmationMessage=`
+                                        Name: ${formData.name}
+                                        Email: ${formData.email}
+                                        Feedback: ${formData.feedback}`;
+            const isConfirmed= window.confirm(`Please confirm your details: \n\n ${confirmationMessage}`);
+            if(isConfirmed){
+                console.log('Submitting Feedback',formData);
+                setFormData({
+                    name:'',
+                    email: '',
+                    feedback: ''
+                });
+                alert('Thank You for your valueable feedback!');
+            }
+        };
 
   return (
     <>
     <nav>
     Tell Us What You Think
     </nav>
-      <form className="feedback-form">
+      <form onSubmit={handleSubmit} className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
-        
+
+        <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder='Your Name' />
+        <input type='email' name='email' value={formData.email} onChange={handleChange} placeholder='Your Email'/>
+        <textarea name='feedback' value={formData.feedback} onChange={handleChange} placeholder='Your Feedback'></textarea>
+
+        <button type='submit'>Submit Feedback</button>
       </form>
     </>
   );
